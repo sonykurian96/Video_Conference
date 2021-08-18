e85815f542bb800551007f974b27d0638328e627
sonykurian96 <sonykurian96@gmail.com>
2021-08-19 00:27:09 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:video_conference/pages/homepage.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage()));
+  runApp(GetMaterialApp(home: HomePage()));
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Video Conference"),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20, 40, 0, 0),
-          child: ElevatedButton.icon(
-            onPressed: () {},
-            icon: Icon(Icons.add),
-            label: Text("New Meeting"),
-            style: ElevatedButton.styleFrom(
-              fixedSize: Size(350, 30),
-              primary: Colors.indigo,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25)),
-            ),
-          ),
-        ),
-        Divider(thickness: 1,height: 40,indent: 40,endIndent: 20,),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-          child: OutlinedButton.icon(
-            onPressed: (){},
-            icon: Icon(Icons.margin),
-            label: Text("Join with a code"),
-            style: OutlinedButton.styleFrom(
-              primary: Colors.indigo,
-              side: BorderSide(color: Colors.indigo),
-              fixedSize: Size(350,30),
-              shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25)),
-            ),
-          ),
-        ),
-        SizedBox(height: 150),
-        Image.network("https://user-images.githubusercontent.com/67534990/127524449-fa11a8eb-473a-4443-962a-07a3e41c71c0.png")
-      ]),
-    );
-  }
-}
 
+
+
+
+// jwc
+// "https://user-images.githubusercontent.com/67534990/127776450-6c7a9470-d4e2-4780-ab10-143f5f86a26e.png",
 
 
 
---
+++ lib/pages/joinWithCode.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class JoinWithCode extends StatelessWidget {
+ 
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: InkWell(
+                child: Icon(Icons.arrow_back_ios_new_sharp, size: 35),
+                onTap: Get.back,
+              ),
+            ),
+            SizedBox(height: 50),
+            Image.network(
+              "https://user-images.githubusercontent.com/67534990/127776450-6c7a9470-d4e2-4780-ab10-143f5f86a26e.png",
+              fit: BoxFit.cover,
+              height: 100,
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Enter meeting code below",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15, 20, 15, 20),
+              child: Card(
+                color: Colors.grey[300],
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+              ),
+                child: TextField(
+                  controller: _controller,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Example : abc-efg-dhi"
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Join"),
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(50, 30),
+                primary: Colors.indigo,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
