2adb197b86316162d0542a2a981cb054f37db982
Sony Kurian <67534990+sonykurian96@users.noreply.github.com>
2021-10-06 15:42:48 +0530

    Update new_meeting.dart

--- lib/pages/new_meeting.dart
+++ lib/pages/new_meeting.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:share/share.dart';
+import 'package:uuid/uuid.dart';
+import 'package:video_conference/video_call.dart';
 
-class NewMeeting extends StatelessWidget {
-  const NewMeeting({Key? key}) : super(key: key);
+class NewMeeting extends StatefulWidget {
+  NewMeeting({Key? key}) : super(key: key);
+
+  @override
+  _NewMeetingState createState() => _NewMeetingState();
+}
+
+class _NewMeetingState extends State<NewMeeting> {
+  String _meetingCode = "abcdfgqw";
+
+  @override
+  void initState() {
+    var uuid = Uuid();
+    _meetingCode = uuid.v1().substring(0, 8);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +28,12 @@ class NewMeeting extends StatelessWidget {
         child: Column(
           children: [
             Align(
-                alignment: Alignment.topLeft,
-                child: InkWell(
-                  child: Icon(Icons.arrow_back_ios_new_sharp, size: 35),
-                  onTap: Get.back,
-                ),
+              alignment: Alignment.topLeft,
+              child: InkWell(
+                child: Icon(Icons.arrow_back_ios_new_sharp, size: 35),
+                onTap: Get.back,
               ),
+            ),
             SizedBox(height: 50),
             Image.network(
               "https://user-images.githubusercontent.com/67534990/127776392-8ef4de2d-2fd8-4b5a-b98b-ea343b19c03e.png",
@@ -31,47 +48,48 @@ class NewMeeting extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.fromLTRB(15, 20, 15, 0),
               child: Card(
-                color: Colors.grey[300],
-                shape: RoundedRectangleBorder(
+                  color: Colors.grey[300],
+                  shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(25),
-              ),
-                child: ListTile(
-                  leading: Icon(Icons.link),
-                  title: SelectableText(
-                    "askdska",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w300
-                    ),
                   ),
-                  trailing: Icon(Icons.copy),
-                )
+                  child: ListTile(
+                    leading: Icon(Icons.link),
+                    title: SelectableText(
+                      _meetingCode,
+                      style: TextStyle(fontWeight: FontWeight.w300),
+                    ),
+                    trailing: Icon(Icons.copy),
+                  )),
+            ),
+            Divider(thickness: 1, height: 40, indent: 20, endIndent: 20),
+            ElevatedButton.icon(
+              onPressed: () {
+                Share.share("Meeting Code : $_meetingCode");
+              },
+              icon: Icon(Icons.arrow_drop_down),
+              label: Text("Share invite"),
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(350, 30),
+                primary: Colors.indigo,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25)),
               ),
             ),
-            Divider(thickness: 1,height: 40,indent:20, endIndent: 20),
-            ElevatedButton.icon(
-            onPressed: () {},
-            icon: Icon(Icons.arrow_drop_down),
-            label: Text("Share invite"),
-            style: ElevatedButton.styleFrom(
-              fixedSize: Size(350, 30),
-              primary: Colors.indigo,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25)),
+            SizedBox(height: 20),
+            OutlinedButton.icon(
+              onPressed: () {
+                Get.to(VideoCall(channelName: _meetingCode.trim()));
+              },
+              icon: Icon(Icons.video_call),
+              label: Text("start call"),
+              style: OutlinedButton.styleFrom(
+                primary: Colors.indigo,
+                side: BorderSide(color: Colors.indigo),
+                fixedSize: Size(350, 30),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25)),
+              ),
             ),
-          ),
-          SizedBox(height:20),
-          OutlinedButton.icon(
-            onPressed: (){},
-            icon: Icon(Icons.video_call),
-            label: Text("start call"),
-            style: OutlinedButton.styleFrom(
-              primary: Colors.indigo,
-              side: BorderSide(color: Colors.indigo),
-              fixedSize: Size(350, 30),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25)),
-            ),
-          ),
           ],
         ),
       ),
